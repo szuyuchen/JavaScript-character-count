c31af00604d6ff390d2f8aecb2f9a3165a8765a8
SYC <112160477+szuyuchen@users.noreply.github.com>
2022-09-25 22:31:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let characterCount = document.getElementById("character-count");
 let wordCount = document.getElementById("word-count");
 
 inputTextArea.addEventListener("input", () => {
-   characCount.textContent = inputTextArea.value.length;
+   characterCount.textContent = inputTextArea.value.length;
 
 let txt = inputTextArea.value.trim();
    wordCount.textContent = txt.split(/\s+/).filter((item) => item).length;
